5247c79a7fb5349d1b50fb3fc3a3a202264e8fb8
sebamarques <sebamarquezblanco@gmail.com>
2024-08-11 23:26:26 -0300

    v1.2

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-document.addEventListener("scroll",()=>{
-    let header = document.querySelector("header")
-    if(window.scrollY>0){
-        header.classList.add("scrolled")
+document.addEventListener("scroll", () => {
+    let header = document.querySelector("header");
+    if (window.scrollY > 0) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
     }
-    else{
-        header.classList.remove("scrolled")
-    }
-})
+
+    const navbarLinks = document.querySelectorAll('header nav a');
+    const sections = document.querySelectorAll('section');
+    let scrollPos = window.scrollY + window.innerHeight / 2;
+
+    sections.forEach((section, index) => {
+        if (scrollPos >= section.offsetTop && scrollPos < section.offsetTop + section.offsetHeight) {
+            navbarLinks.forEach(link => link.classList.remove('active'));
+            navbarLinks[index].classList.add('active');
+        }
+    });
+});
